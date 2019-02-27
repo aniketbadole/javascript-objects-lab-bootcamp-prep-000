1f83f5605241ed9109f822af022a4e5a5420e58a
Aniket B <aniket.b@outlook.com>
2019-02-27 13:47:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 var recipes = {}
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, { [key]: value })
+ function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value})
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
-  return object
+  return objec
 }
 
-function deleteFromObjectByKey(object, key) {
-  var cloneObj = Object.assign({}, object)
-  delete cloneObj.key
-  return cloneObj
+ function deleteFromObjectByKey(object, key) {
+  var clone = Object.assign({}, object);
+  delete clone[key]
+  return clone
 }
 
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key
+ function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
   return object
 }
-     
