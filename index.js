ccdeeba9d70fa47d0d24911ba2b966cc59767daf
Aniket B <aniket.b@outlook.com>
2019-02-27 13:39:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,3 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
-
-function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, obj)
-  delete newObj.key
-  return newObj
-}
