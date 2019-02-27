10f1643cf36c17a552120ae68c3978722be2d158
Aniket B <aniket.b@outlook.com>
2019-02-27 13:31:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = { } //foo: 'bar'}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  //return recipes
+  return recipes
   return Object.assign({}, object, { [key]: value })
 }
 
